d8d71fa59a8650ce2d46df0b941a42f7ecbe5c43
Benjamin Leclerc <benjamin.leclerc.ext@eurocontrol.int>
2019-09-24 11:47:10 +0000

    Auto reconnect, use any ioredis options

--- src/client.js
+++ src/client.js
@@ -4,38 +4,58 @@ const WebSocket = require('ws');
 
 const redisClient = createRedisClient();
 
-const instanceId = nconf.get('instanceId');
-const secret = nconf.get('secret');
+const instance = nconf.get('instance');
+
+if (!instance) {
+    console.error('The instance configuration is missing');
+    return;
+}
+
+const instanceId = nconf.get('instance:id');
+const secret = nconf.get('instance:secret');
 
 if (!instanceId || !secret) {
     console.error('The instance id and/or the secret are missing');
     return;
 }
 
-const ws = new WebSocket(`ws://localhost:5000/socket?instanceId=${instanceId}&secret=${secret}`);
+function connect() {
+    const ws = new WebSocket(`ws://localhost:5000/socket?instanceId=${instanceId}&secret=${secret}`);
 
-let interval;
+    let interval;
 
-ws.on('open', function open() {
-    interval = setInterval(async () => {
-        const info = await redisClient.info();
+    ws.on('open', function open() {
+        console.log("Connected");
 
-        ws.send(JSON.stringify({
-            action: 'info',
-            data: info
-        }));
-    }, 5000);
-});
+        interval = setInterval(async () => {
+            const info = await redisClient.info();
 
-ws.on('close', function open() {
-    clearInterval(interval);
-    interval = null;
-});
+            ws.send(JSON.stringify({
+                action: 'info',
+                data: info
+            }));
+        }, 5000);
+    });
 
-ws.on('message', function message(data) {
-    console.log(data);
-});
+    ws.on('close', function open() {
+        clearInterval(interval);
+        interval = null;
 
-ws.on('error', function error(error) {
-    console.error(error.toString());
-});
+        console.log("Connection closed. Trying to reconnect...");
+
+        setTimeout(function () {
+            connect();
+        }, 2000);
+    });
+
+    ws.on('message', function message(data) {
+        console.log(data);
+    });
+
+    ws.on('error', function error(error) {
+        console.error(error.toString());
+        ws.close();
+    });
+}
+
+connect();
--- src/redis.js
+++ src/redis.js
@@ -2,24 +2,14 @@ const { nconf } = require('./config');
 const Redis = require('ioredis');
 
 function createRedisClient() {
-    if (!nconf.get('redis:host')) {
-        throw new Error('Redis host is missing');
-    }
-
-    if (!nconf.get('redis:port')) {
-        throw new Error('Redis port is missing');
+    if (!nconf.get('redis')) {
+        throw new Error('Redis options are missing');
     }
 
     const options = {
-        host: nconf.get('redis:host'),
-        port: nconf.get('redis:port'),
-        db: nconf.get('redis:db'),
-        enableOfflineQueue: false
+        enableOfflineQueue: false,
+        ...nconf.get('redis')
     };
-
-    if (nconf.get('redis:password')) {
-        options.password = nconf.get('redis:password');
-    }
 
     return new Redis(options);
 }
