fd52e0d2d95962f139829db48009fb3d3c532bce
Benjamin Leclerc <benjamin.leclerc@me.com>
2019-10-13 12:03:10 +0200

    Move websocket to separate file

--- src/client.js
+++ src/client.js
@@ -1,8 +1,8 @@
 const { URL } = require('url');
-const WebSocket = require('ws');
 const { nconf } = require('./config');
 const { getAgent } = require('./proxy');
 const { createRedisClient } = require('./redis');
+const { connectWebSocket } = require('./websocket');
 
 const redisClient = createRedisClient();
 
@@ -28,74 +28,4 @@ url.searchParams.set('secret', secret);
 const agent = getAgent();
 const options = agent ? { agent } : {};
 
-function connect() {
-    const ws = new WebSocket(url, options);
-
-    let interval;
-
-    ws.on('open', function open() {
-        console.log("Connected");
-
-        interval = setInterval(async () => {
-            ws.send(JSON.stringify({
-                messageId: 'status',
-                status: 200,
-                data: redisClient.status
-            }));
-
-            const info = await redisClient.info();
-
-            if (redisClient.status === 'ready') {
-                ws.send(JSON.stringify({
-                    messageId: 'info',
-                    status: 200,
-                    data: info
-                }));
-            }
-        }, 5000);
-    });
-
-    ws.on('close', function open() {
-        clearInterval(interval);
-        interval = null;
-
-        console.log("Connection closed. Trying to reconnect...");
-
-        setTimeout(function () {
-            connect();
-        }, 10000);
-    });
-
-    ws.on('message', async function message(data) {
-        try {
-            const message = JSON.parse(data);
-
-            if ('messageId' in message) {
-                if (redisClient.status === 'ready') {
-                    const result = await redisClient.call(message.command, message.parameters);
-
-                    ws.send(JSON.stringify({
-                        messageId: message.messageId,
-                        status: 200,
-                        data: result
-                    }));
-                } else {
-                    ws.send(JSON.stringify({
-                        messageId: message.messageId,
-                        status: 405,
-                        data: null
-                    }));
-                }
-            }
-        } catch (e) {
-            // Skip message
-        }
-    });
-
-    ws.on('error', function error(error) {
-        console.error(error.toString());
-        ws.close();
-    });
-}
-
-connect();
+connectWebSocket(url, options, redisClient);
